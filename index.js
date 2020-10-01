5ad8ead482c76e542abda1bf418f6cdfd42a0322
smarten <smarten.developer@gmail.com>
2020-09-30 21:03:58 -0700

    Created post and get routes for users

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(routes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes); // Implicit '/' as the first arg
 
 app.listen(3001, () => {
   console.log('Server started listening on PORT http://localhost:3001');
--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
+const connection = require('../../../config/connection');
 
 // /api/users prepended to every Route
 router.route('/')
-  .get((_req, res) => {
-    res.send('Hello');
+  .get(async (req, res) => {
+    const query = 'SELECT * FROM users;';
+    const [rows, fields] = await connection.query(query);
+    console.log('I AM ROWS', rows);
+    console.log('I AM FIELDS', fields);
+    res.json(rows);
+  })
+  .post(async (req, res) => {
+    const userInput = req.body;
+    const query = 'INSERT INTO users SET ?';
+    const result = await connection.query(query, userInput);
+    res.json(result);
   });
 
 module.exports = router;
